264fa39e38ed0d3756a049365329f241159f12ce
Rahima Sohail <buttrahima34@gmail.com>
2025-12-24 12:38:09 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,58 @@
 
-//import HomeFirstSection from "../Components/HomeSections/HomeFirstSection";
+// //import HomeFirstSection from "../Components/HomeSections/HomeFirstSection";
+// import HomeSecondSection from "../Components/HomeSections/HomeSecondSection";
+// //import HomeThirdSection from "../Components/HomeSections/HomeThirdSection";
+// import HomeFourthSection from "../Components/HomeSections/HomeFourthSection";
+// import HomeFifthSection from "../Components/HomeSections/HomeFifthSection";
+// import ServicesSection from "../Components/HomeSections/ServicesSection";
+
+
+// export default function Home() {
+//   return (
+//     <>
+//       {/* <HomeFirstSection /> */}
+//       <HomeSecondSection />
+//       <ServicesSection />
+//       {/* <HomeThirdSection /> */}
+//       <HomeFourthSection />
+//       <HomeFifthSection />
+      
+//     </>
+//   );
+// }
+
+
+// pages/index.tsx or app/page.tsx
+
 import HomeSecondSection from "../Components/HomeSections/HomeSecondSection";
-//import HomeThirdSection from "../Components/HomeSections/HomeThirdSection";
 import HomeFourthSection from "../Components/HomeSections/HomeFourthSection";
 import HomeFifthSection from "../Components/HomeSections/HomeFifthSection";
 import ServicesSection from "../Components/HomeSections/ServicesSection";
 
-
 export default function Home() {
+  // Common style to reduce vertical spacing
+  const sectionStyle = {
+    margin: 0,
+    padding: 0
+  };
+
   return (
     <>
-      {/* <HomeFirstSection /> */}
-      <HomeSecondSection />
-      <ServicesSection />
-      {/* <HomeThirdSection /> */}
-      <HomeFourthSection />
-      <HomeFifthSection />
-      
+      <div style={sectionStyle}>
+        <HomeSecondSection />
+      </div>
+
+      <div style={sectionStyle}>
+        <ServicesSection />
+      </div>
+
+      <div style={sectionStyle}>
+        <HomeFourthSection />
+      </div>
+
+      <div style={sectionStyle}>
+        <HomeFifthSection />
+      </div>
     </>
   );
 }
