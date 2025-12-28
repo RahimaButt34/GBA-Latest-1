e5f8614b16070758e140c6f7d28d3caf063b3245
Rahima Sohail <buttrahima34@gmail.com>
2025-12-27 17:26:14 -0800

    Update Events Section Hoe with Apis

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ['gba.mig.org.pk'],
-  },
+  
+  
 };
 
 export default nextConfig;
